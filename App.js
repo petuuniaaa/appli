c3197b00304f2c89c30d943ef7d94bb96c4cb299
petuuniaaa <celine.debruijne@gmail.com>
2019-02-23 16:03:27 +0100

    #Chargement

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-import Navigation from './Navigation/Navigation'
-import { Provider } from 'react-redux'
-import Store from './Store/configureStore'
+import Accueil from './Components/Accueil'
+
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={Store}>
-        <Navigation />
-      </Provider>
+      // <Accueil />
     )
   }
 }
---
+++ Components/Chargement/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, View, Image } from 'react-native'
+
+class Chargement extends React.Component {
+    RedirectionJavascript() {
+    }
+
+    render() {
+        return (
+            <View style={design.main_container} onLoad={() => setTimeout(() => 3500)}>
+                <Image
+                    source={require('../../Images/ako.png')}
+                    style={design.image}
+                />
+            </View>
+        )
+    }
+}
+
+const design = StyleSheet.create({
+    main_container: {
+        backgroundColor: '#41BABE',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    image: {
+        width: 200,
+        height: 200,
+    }
+})
+
+export default Chargement
